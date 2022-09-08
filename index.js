98b2e0daddb5f77f8fbfbccf76792a8d844813c3
Belkbh <belkbh@gmail.com>
2022-09-07 22:35:55 -0400

    Did most of the generate markdown portion

--- index.js
+++ index.js
@@ -5,67 +5,71 @@ const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [
     {
-        name: 'repoTitle',
+        name: 'title',
         message: 'Enter a title for your GitHub repository',
         default: 'README-PROJECT',
     },
     {
-        name: 'repoDescription',
+        name: 'description',
         message: 'Enter a description for your README',
         default: 'Default Description',
     },
     {
-        name: 'repoInstallation',
+        name: 'installation',
         message: 'Enter the instructions for the installation of your project',
         default: 'Default instrucitons',
     },
     {
-        name: 'repoUsage',
+        name: 'usage',
         message: 'Enter the usage information for your project',
         default: 'Default usage',
     },
     {
-        name: 'repoContributing',
+        name: 'contributing',
         message: 'Enter information on how to contribute to this project',
         default: 'Default contributers',
     },
     {
-        name: 'repoTests',
+        name: 'tests',
         message: 'Enter the information on tests for your project',
         default: 'Default tests',
     },
     {
-        name: "repoLicense",
+        name: "license",
         type: 'list',
         message: 'Select a license for your project',
         choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT Liecense', 'Boost Software License 1.0', 'The Unlicense'],
     },
     {
-        name: 'repoGitHubName',
+        name: 'gitHubName',
         message: 'Enter your GitHub username to direct questions about your project',
     },
     {
-        name: 'repoEmail',
+        name: 'email',
         message: 'Enter your email to direct questions about your project',
     },
     ];
     
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+var getMarkdown = generateMarkdown(data);
+console.log(getMarkdown);
+}
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions)
     .then(function(data){
-        const newTitle = data.repoTitle;
-        const newDescription = data.repoDescription;
-        const newInstallation = data.repoInstallation;
-        const newUsage = data.repoUsage;
-        const newContributing = data.repoContributing;
-        const newTests = data.repoTests;
+        // const newTitle = data.repoTitle;
+        // const newDescription = data.repoDescription;
+        // const newInstallation = data.repoInstallation;
+        // const newUsage = data.repoUsage;
+        // const newContributing = data.repoContributing;
+        // const newTests = data.repoTests;
         
-        console.log(data);
+        // console.log(data);
+        writeToFile("README.md", data);
     });
 }
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,64 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license = ""){return "";}
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license = ""){return "";}
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license = ""){return "";}
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-  ## Fun Times
+  <br />
+  ## Table of contents
+  1. [Description](#description)
+  2. [Installation](#installation)
+  2. [Usage](#usage)
+  2. [Contributing](#contributing)
+  2. [Tests](#tests)
+  2. [License](#license)
+  2. [Questions](#Questions)
+
+  ## Description
+  <br />
+  ${data.description}
+  <br />
+  ## Installation
+  <br />
+  ${data.installation}
+  <br />
+  ## Usage
+  <br />
+  ${data.usage}
+  <br />
+  ## Contributing
+  <br />
+  ${data.contributing}
+  <br />
+  ## Tests
+  <br />
+  ${data.tests}
+  <br />
+  ## License
+  <br />
+  License stuff goes here
+  ${renderLicenseSection(data.license)}
+  [License information available here](${renderLicenseLink(data.license)})
+  <br />
+  ## Questions
+  <br />
+  Direct any questions to the github account of ${data.githubName} or to the email ${data.email}. Thank you.
+  <br />
 `;
 }
 
